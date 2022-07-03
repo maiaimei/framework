222c974bf5b46b0537191491323b240503d365f9
maiaimei <1211674185@qq.coom>
2022-07-03 20:56:06 +0800

    refactor code

--- mybatis-plus-spring-boot-starter/src/main/java/cn/maiaimei/framework/mybatis/plus/util/PageUtils.java
+++ mybatis-plus-spring-boot-starter/src/main/java/cn/maiaimei/framework/mybatis/plus/util/PageUtils.java
@@ -1,19 +1,19 @@
 package cn.maiaimei.framework.mybatis.plus.util;
 
-import cn.maiaimei.framework.beans.PageResult;
+import cn.maiaimei.framework.beans.PagingResult;
 import cn.maiaimei.framework.util.BeanUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
 import java.util.List;
 
 public class PageUtils {
-    public static <T> PageResult getPageResult(Page page, Class<T> targetClass) {
+    public static <T> PagingResult generate(Page page, Class<T> targetClass) {
         List<T> records = BeanUtils.copyList(page.getRecords(), targetClass);
-        PageResult<T> pageResult = new PageResult<>();
-        pageResult.setRecords(records);
-        pageResult.setTotal(page.getTotal());
-        pageResult.setCurrent(page.getCurrent());
-        pageResult.setSize(page.getSize());
-        return pageResult;
+        PagingResult<T> pagingResult = new PagingResult<>();
+        pagingResult.setRecords(records);
+        pagingResult.setTotal(page.getTotal());
+        pagingResult.setCurrent(page.getCurrent());
+        pagingResult.setSize(page.getSize());
+        return pagingResult;
     }
 }
